921b5d8b19e05be7bc0e95ebdf119d496c675c0d
Junior Melo <juniormelo.dev@gmail.com>
2023-01-04 13:43:37 -0300

    Adicionando projeto Java WEB - Ageda de Contatos
    Projeto WEB com Java, pelo site Workover e ensinamentos do Professor José de Assis.
    https://www.youtube.com/@ProfessorJosedeAssis

---
+++ agenda/src/main/webapp/scripts/confirmador.js
@@ -0,0 +1,13 @@
+/**
+ * Confirmação de exclusão de um contato
+ 
+ * @author Junior Melo
+ * @param idcon
+ */
+ 
+ function confirmar(idcon) {
+	let resposta = confirm("Você confirma a exclusão deste contato ?")
+	if (resposta === true){
+		window.location.href = "delete?idcon" + idcon
+	}
+}
